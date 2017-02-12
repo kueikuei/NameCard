55245df5f18246713985b3ae93ab21ba78e97a7c
KueiKuei <r03228001@ntu.edu.tw>
2017-02-12 16:58:07 +0800

    first commit

---
+++ namecard.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".face").hover(function(){
+        $(".namecard").css("height","350px");
+        }, function(){
+        $(".namecard").css("height","0px");
+    });
+});
